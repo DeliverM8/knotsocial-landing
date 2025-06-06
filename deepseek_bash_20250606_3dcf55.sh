ae68e388c0966fb57f37a0d00ac1c60e5503b20a
THE Scaffolder <ejware@gmail.com>
2025-06-06 20:02:50 +1000

    Add files via upload

---
+++ deepseek_bash_20250606_3dcf55.sh
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "Initial landing page"
+git remote add origin https://github.com/KnotSocial/knot-landing.git
+git push -u origin main
---
+++ deepseek_javascript_20250606_b60f76.js
@@ -0,0 +1,5 @@
+// Waitlist form submission
+document.querySelector('form')?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thanks! We’ll notify you soon.');
+});
